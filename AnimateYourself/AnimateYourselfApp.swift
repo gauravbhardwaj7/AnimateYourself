21064466e26e4d56555b61a87d942b4000b3b512
gauravbhardwaj7 <106892217+gauravbhardwaj7@users.noreply.github.com>
2023-07-18 22:40:30 +0530

    Initial Commit

---
+++ AnimateYourself/AnimateYourselfApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimateYourselfApp.swift
+//  AnimateYourself
+//
+//  Created by Gaurav Bhardwaj on 18/07/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimateYourselfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
